beb27dd3f72bcd4fdb0ba0aa81764303fe4708e1
王星 <wangxing@wangxingdeMacBook-Air.local>
2022-11-09 09:43:55 +0800

    feat: update

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ import indexModuleCss from './componentA.module.css'
 console.log('main js')
 
 const div = document.createElement('div')
+div.innerHTML = 'hello world'
 document.body.appendChild(div)
 div.className = indexModuleCss
